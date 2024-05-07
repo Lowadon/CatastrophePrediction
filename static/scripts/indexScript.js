ea90d5dfd9f89bc83baad5f7c983dea065a4b00d
Benjamin Schick | fpt <118367850+BenjaminSchickfpt@users.noreply.github.com>
2024-05-07 18:54:48 +0200

    hm

--- static/scripts/indexScript.js
+++ static/scripts/indexScript.js
@@ -1,4 +1,3 @@
-import Chart from 'chart.js/auto'
 // List entries in database
 /*
 $(document).ready(function () {
@@ -19,32 +18,24 @@ $(document).ready(function () {
 });
 */
 
+const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-    
-(async function() {
-const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-];
-
-new Chart(
-    document.getElementById('acquisitions'),
-    {
-    type: 'bar',
-    data: {
-        labels: data.map(row => row.year),
-        datasets: [
-        {
-            label: 'Acquisitions by year',
-            data: data.map(row => row.count)
-        }
-        ]
+new Chart("acquisitions", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor:"rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options:
+  {
+    legend: {display: false},
+    scales: {
+    yAxes: [{ticks: {min: 6, max:16}}], 
     }
-    }
-);
-})();
+}
+});
