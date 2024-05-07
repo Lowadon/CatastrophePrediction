29e81b38c48a6727bd21f97cc6a210fe5a2b2672
Benjamin Schick | fpt <118367850+BenjaminSchickfpt@users.noreply.github.com>
2024-05-07 19:16:34 +0200

    whee

--- static/scripts/indexScript.js
+++ static/scripts/indexScript.js
@@ -1,23 +1,6 @@
 // List entries in database
-/*
-$(document).ready(function () {
-    $.ajax({
-        url: "/getSystemInfo",
-        method: "GET",
-        dataType: "json",
-        success: function (data) {
-            data.forEach(function (entry) {
-                const entryHtml = createEntry(entry);
-                $("#sysInfoList").append(entryHtml);
-            });
-        },
-        error: function (error) {
-            console.error("Error fetching data for List 1: " + error.responseText);
-        },
-    });
-});
-*/
 
+/*
 const xValues = [50,60,70,80,90,100,110,120,130,140,150];
 const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
@@ -39,3 +22,4 @@ new Chart("acquisitions", {
     }
 }
 });
+*/
